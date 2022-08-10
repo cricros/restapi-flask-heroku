d391edb552cbe4413a5d6c4ee306c6da097eeb0d
cricros <vce11987@innovaccion.mx>
2022-08-09 23:33:36 -0500

    third commit

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from flask import Flask
+import requests, json
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/all', methods = ['GET'])
 def home():
-    return "Flask heroku v2"
+    """Retorna todos los pokemon"""
+    data = []
+    name_url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    while True:
+        resp = requests.get(name_url)
+        json = resp.json()
+        data.extend(json.get('results', []))
+        name_url = json.get('next')
+        if not name_url: break
+    return jsonify(data)
+
+@app.route('/pokemon/<string:name>', methods = ['GET'])
+def pokemonName(name):
+    """Para buscar un pokemon en especifico"""
+    name_url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    result = requests.get(name_url)
+    resultJson = result.json()
+    return jsonify(resultJson)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug= True)

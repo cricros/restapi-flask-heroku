6ed282440dd08f590989367ceeee303177aa45e6
cricros <vce11987@innovaccion.mx>
2022-08-12 04:48:22 -0500

    errores commit

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from flask import Flask, jsonify, request, redirect
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-#las siguientes rutas corresponden a la pokeapi
+# las siguientes rutas corresponden a la pokeapi
+
+
+@app.errorhandler(404)
+def pageNotfound(e):
+    return jsonify({"Error ": "la pagina no existe, ingresa una direccion valida"})
+
+@app.errorhandler(405)
+def pageNotfound(e):
+    return jsonify({"Error ": "verifica el tipo de metodo"})
+
 
 @app.route('/')
 def homeRESTAPI():
@@ -18,11 +28,13 @@ def homeRESTAPI():
         <p> Recuerda utilizar un restclient para las peticiones POST </p>
 """
 
+
 @app.route('/restdoc')
 def docRESTAPI():
     return redirect("https://documenter.getpostman.com/view/18771793/VUjSENtP")
 
-@app.route('/all', methods = ['GET'])
+
+@app.route('/all', methods=['GET'])
 def allPokemonGET():
     """Retorna todos los pokemon"""
     data = []
@@ -32,87 +44,90 @@ def allPokemonGET():
         json = resp.json()
         data.extend(json.get('results', []))
         name_url = json.get('next')
-        if not name_url: break
+        if not name_url:
+            break
     return jsonify(data)
 
-@app.route('/pokedex', methods = ['POST'])
+
+@app.route('/pokedex', methods=['POST'])
 def pokedex():
     """Generar respuesta JSON mediante envio de solicitud mediante postman por el body"""
     if request.method == 'POST':
         name = request.json['name']
         name_url = f"https://pokeapi.co/api/v2/pokedex/{name}"
         result = requests.get(name_url)
-        resultJson  = result.json()
+        resultJson = result.json()
         return jsonify(resultJson)
 
-@app.route('/pokedex/<string:name>', methods = ['GET'])
+
+@app.route('/pokedex/<string:name>', methods=['GET'])
 def pokedexGET(name):
     """Generar respuesta JSON mediante envio de parametros"""
     if request.method == 'GET':
         name_url = f"https://pokeapi.co/api/v2/pokedex/{name}"
         result = requests.get(name_url)
-        resultJson  = result.json()
+        resultJson = result.json()
         return jsonify(resultJson)
 
-@app.route('/pokemon', methods = ['POST'])
+
+@app.route('/pokemon', methods=['POST'])
 def pokemonName():
     """Generar respuesta JSON mediante envio de solicitud mediante postman por el body"""
     if request.method == 'POST':
         name = request.json['name']
         name_url = f"https://pokeapi.co/api/v2/pokemon/{name}"
         result = requests.get(name_url)
-        resultJson  = result.json()
+        resultJson = result.json()
         return jsonify({
-        "id":resultJson['id'],
-        "name":resultJson['name'],
-        "types":resultJson['types'],
-        "stats":resultJson['stats'], 
-        "game_indices":resultJson['game_indices']})
+            "id": resultJson['id'],
+            "name": resultJson['name'],
+            "types": resultJson['types'],
+            "stats": resultJson['stats'],
+            "game_indices": resultJson['game_indices']})
 
-@app.route('/pokemon/<string:name>', methods = ['GET'])
+
+@app.route('/pokemon/<string:name>', methods=['GET'])
 def pokemonNameGET(name):
     """Para buscar un pokemon en especifico"""
     name_url = f"https://pokeapi.co/api/v2/pokemon/{name}"
     result = requests.get(name_url)
     resultJson = result.json()
     return jsonify({
-        "id":resultJson['id'],
-        "name":resultJson['name'],
-        "types":resultJson['types'],
-        "stats":resultJson['stats'], 
-        "game_indices":resultJson['game_indices']
+        "id": resultJson['id'],
+        "name": resultJson['name'],
+        "types": resultJson['types'],
+        "stats": resultJson['stats'],
+        "game_indices": resultJson['game_indices']
     })
 
-@app.route('/type', methods = ['POST'])
+
+@app.route('/type', methods=['POST'])
 def pokemonType():
     """Para buscar un typo pokemon en especifico"""
     if request.method == 'POST':
         type = request.json['type']
         name_url = f"https://pokeapi.co/api/v2/type/{type}"
         result = requests.get(name_url)
-        resultJson  = result.json()
+        resultJson = result.json()
         return jsonify({
-        "id":resultJson['id'],
-        "name":resultJson['name'],
-        "game_indices":resultJson['game_indices']
-    })
+            "id": resultJson['id'],
+            "name": resultJson['name'],
+            "game_indices": resultJson['game_indices']
+        })
 
-@app.route('/type/<string:nametype>', methods = ['GET'])
+
+@app.route('/type/<string:nametype>', methods=['GET'])
 def pokemonTypeGET(nametype):
     """Para buscar un pokemon en especifico"""
     name_url = f"https://pokeapi.co/api/v2/type/{nametype}"
     result = requests.get(name_url)
     resultJson = result.json()
     return jsonify({
-        "id":resultJson['id'],
-        "name":resultJson['name'],
-        "game_indices":resultJson['game_indices']
+        "id": resultJson['id'],
+        "name": resultJson['name'],
+        "game_indices": resultJson['game_indices']
     })
 
-def handle_error_404(error):
-    return "<h1>No existe...</h1>"
-    
+
 if __name__ == '__main__':
-    app.run(debug= True)
-    app.register_error_handler(404, handle_error_404)
-    
+    app.run(debug=True)
